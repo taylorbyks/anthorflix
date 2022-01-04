fef2d1e94d65ede5e9dcda1b0fe4f99c0e7bb8c7
Taylor Schinvelski <taylorbyks@hotmail.com>
2022-01-04 20:46:21 -0300

    fix: logo

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
 
 export function Logo() {
-  return <Image src="/logo.png" alt="anthorflix" width="339" height="86"/>
+  return <Image src="/logo.png" alt="anthorflix" width="280" height="70"/>
 }
