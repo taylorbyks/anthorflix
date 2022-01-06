c2ba477a44ebc8107c4dc6c878243e8b0cc86694
Taylor Schinvelski <taylorbyks@hotmail.com>
2022-01-05 22:59:53 -0300

    feat: add comment

--- frontend/src/models/rating.ts
+++ frontend/src/models/rating.ts
@@ -1,8 +1,17 @@
 import { User } from "./user";
 
 export interface Rating {
+  id: string
   movie: string
   score: number
   review: string
+  created_at: Date
+  comments: Comment[]
   user: User
+}
+
+interface Comment {
+  user: User
+  message: string
+  date: Date
 }
